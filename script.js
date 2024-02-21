649d2fe141d5916f1e123ebda4dbd683a7b2cf85
Chhavi <guptachhavi880@gmail.com>
2024-02-22 01:05:52 +0530

    Added js for switchign tabs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//--------------------Js to scroll content-----------------------------
 let lastScrollTop = 0;
 
 window.addEventListener("scroll", function () {
@@ -9,3 +10,28 @@ window.addEventListener("scroll", function () {
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
+
+//-----------------------Js to switch tabs----------------------------
+document.querySelector('.selectedTab').classList.add('selectedTab');
+
+// Function to toggle content and tabs
+function showContent(tab) {
+    document.querySelectorAll('.contentBox').forEach(content => {
+        content.style.display = 'none';
+    });
+
+    document.querySelectorAll('.desc, .har, .use').forEach(tab => {
+        tab.classList.remove('selectedTab');
+    });
+
+    if (tab === 'description') {
+        document.getElementById('descriptionContent').style.display = 'block';
+        document.querySelector('.desc').classList.add('selectedTab');
+    } else if (tab === 'harvest') {
+        document.getElementById('harvestContent').style.display = 'block';
+        document.querySelector('.har').classList.add('selectedTab');
+    } else if (tab === 'useCases') {
+        document.getElementById('useCasesContent').style.display = 'block';
+        document.querySelector('.use').classList.add('selectedTab');
+    }
+}
